9b837549fcd3b7b033928ac5fd81481a3a052c22
Victory <tuduov@gmail.com>
2020-11-19 18:14:02 +0100

    second upload
    add eye over effect in home section, added submission of forms . added a what i can do for you section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,77 @@ sr.reveal('.about__text', {delay:400})
 
 //scroll skills
 sr.reveal('.skills__subtitle', {})
-sr.reveal('.button', {delay:200})
+sr.reveal('.skills__text', {delay:200})
+sr.reveal('.skills__data', {interval:200})
+sr.reveal('.skills__img', {delay:400})
 
 //scroll work
+sr.reveal('.work__img', {interval:200})
+
+
 //scroll contact
+sr.reveal('.contact__input', {interval:200})
+
+
+//addition
+document.querySelector('.home').addEventListener('mouseover', eyeball);
+
+function eyeball(){
+    const eye = document.querySelector(".rotating");
+    let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+    let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let rotation = (radian * (180 / Math.PI) * -1) +270;
+    eye.style.transform = "rotate("+rotation+"deg)"
+}
+
+
+//more addition form sumitting
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("success");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      status.classList.add('suc');
+    //   button.style = "display: none ";
+      status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+      status.classList.add('err');
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
